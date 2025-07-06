7d4bc4eb6d6949581740937a436121afe8f5ff06
Riddhima Kushwah <riddhimakushwah5052@gmail.com>
2025-07-06 21:21:58 +0530

    StudentMarksCalculator.java

--- StudentMarksCalculator.java
+++ StudentMarksCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StudentGradeCalculator {
+public class StudentMarksCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
